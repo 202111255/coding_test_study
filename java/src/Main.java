9d56b76901ee8fc3d030ac8a1d5ce47c518b67cc
강바다 <kbd1120@naver.com>
2024-09-16 17:06:36 +0900

    무인도 문제

--- java/src/Main.java
+++ java/src/Main.java
@@ -7,23 +7,39 @@ public class Main {
     public static void main(String[] args) {
 
         Solution so = new Solution();
-        int[] arr1 = {1, 5, 2, 6, 3, 7, 4};
-        int[][] arr2 = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
-
-        System.out.println(so.solution(arr1, arr2));
+        String[] arr1 = {"X591X","X1X5X","X231X", "1XXX1"};
+        //int[][] arr2 = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+        System.out.println(so.solution(arr1));
     }
 }
 
 class Solution {
-    public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
-        int i = 0;
-        for (int[] command : commands) {
-            int[] slicedArray = Arrays.copyOfRange(array, command[0]-1, command[1]);
-            slicedArray = Arrays.stream(slicedArray).sorted().toArray();
-            answer[i] = slicedArray[command[2]-1];
-            i++;
+    public int[] solution(String[] maps) {
+        int[] answer = {};
+        //int[][]형 배열로 만들기
+        int[][] new_maps = new int[maps.length][maps[0].length()];
+        for (int i = 0; i < maps.length; i++) {
+            for (int j = 0; j < maps[i].length(); j++) {
+                if (maps[i].charAt(j) == 'X') {
+                    new_maps[i][j] = -1;
+                } else {
+                    new_maps[i][j] = Integer.parseInt(Character.toString(maps[i].charAt(j)));
+                }
+            }
         }
+
+        Queue<Integer> queue = new ArrayDeque<>();
+        int sum = 0
+        for (int i = 0; i < new_maps.length; i++) {
+            for (int j = 0; j < new_maps[0].length; j++) {
+                if (new_maps[i][j] != -1) {
+                    //오른쪽과 아래 확인
+                    queue.add(new_maps[i][j]);
+                }
+
+            }
+        }
+
 
         return answer;
     }
